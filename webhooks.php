faa282cf4b1d80c93fd120262f031c13597c528c
Panupng Subsri <panupong.su@senate.co.th>
2021-02-15 15:49:05 +0700

    Edit access_token

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'BCOOz/CuGPNdzX413qDxHZZSA+cYRh94Jb5GfrSr1/+nMQqNvT50EY5WKrQ2NjxMlNOWul1AtlS6gNgS+iwG5K/L39uHzhnOAvryeeBZdldlcBdhTcHnCP21l4fYKWVnkZDyS7idnJKLgL/j2M0vIwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
