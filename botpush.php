fa064e0cbf73a02161576f42c90fdb19a29071c2
Panupng Subsri <panupong.su@senate.co.th>
2021-02-15 17:08:02 +0700

    Edit text

--- botpush.php
+++ botpush.php
@@ -13,7 +13,7 @@ $pushID = 'Uee2b386243d3bc4b8020998b2d81bf15';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Kuy');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
